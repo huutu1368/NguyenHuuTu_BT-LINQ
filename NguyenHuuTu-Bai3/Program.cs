744360d9caa8d772f94dd94d627478fece371cf0
Huu Tu <huutu1368@gmail.com>
2026-03-20 20:33:51 +0700

    NguyenHuuTunopbaitap3

---
+++ NguyenHuuTu-Bai3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Bai3
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nguyen Huu Tu // 2415053122346 // 225LTC03");
+        List<int> numbers = new List<int> { 8,7,6,5,4,3,2,1 };
+        Console.WriteLine("Danh sach truoc khi sap xep la:");
+        foreach (var sc in numbers)
+        {
+            Console.Write(sc + " ");
+        }
+        Console.WriteLine();
+        var ds = numbers.OrderBy(p => p);
+        Console.WriteLine("Danh sach sau khi sap xep tang dan la:");
+        foreach (var sc in ds)
+        {
+            Console.Write(sc + " ");
+        }
+    }
+}
