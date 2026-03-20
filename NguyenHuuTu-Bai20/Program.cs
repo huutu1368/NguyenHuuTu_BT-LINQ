fd73fde37ef1876ff8e6ba35247c94bf494dff7b
Huu Tu <huutu1368@gmail.com>
2026-03-20 21:10:17 +0700

    NguyenHuuTunopbaitap20

---
+++ NguyenHuuTu-Bai20/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Score { get; set; }
+}
+
+class Bai20
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nguyen Huu Tu // 2415053122346 // 225LTC03");
+        List<Student> students = new List<Student>() {
+            new Student { Id = 1, Name = "An", Score = 8 },
+            new Student { Id = 2, Name = "Binh", Score = 6 },
+            new Student { Id = 3, Name = "Chi", Score = 9 },
+            new Student { Id = 4, Name = "Dung", Score = 7 } };
+        Console.WriteLine("Danh sach sinh vien:");
+        foreach (Student sv in students)
+            Console.WriteLine($"ID={sv.Id,-5} || Name={sv.Name,-10} || Score={sv.Score,-5}");
+        var svv = students.OrderByDescending(sv => sv.Score);
+        var top3sv = svv.Take(3);
+        Console.WriteLine("Thong tin sinh vien co top 3 diem cao nhat:");
+        foreach (var svvv in top3sv)
+            Console.WriteLine($"ID={svvv.Id,-5} || Name={svvv.Name,-10} || Score={svvv.Score,-5}");
+    }
+}
