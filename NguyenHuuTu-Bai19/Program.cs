be8e1cdc5df8f89b4a4172ccbe7f559a7dfdbf0f
Huu Tu <huutu1368@gmail.com>
2026-03-20 21:08:29 +0700

    NguyenHuuTunopbaitap19

---
+++ NguyenHuuTu-Bai19/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Bai19
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nguyen Huu Tu // 2415053122346 // 225LTC03");
+        List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 2, 5 };
+        Console.WriteLine("Danh sach cac phan tu:");
+        foreach (var pt in numbers)
+            Console.Write(pt + " ");
+        var group = numbers.GroupBy(pt => pt);
+        var ptt = group.OrderByDescending(gr => gr.Count()).FirstOrDefault();
+        if (ptt != null)
+            Console.WriteLine($"\nPhan tu {ptt.Key} xuat hien nhieu nhat voi {ptt.Count()} lan.");
+    }
+}
