0055f5cfe1eedd9127976d8e7c1b5e98e25d3f9d
Huu Tu <huutu1368@gmail.com>
2026-03-20 20:37:54 +0700

    NguyenHuuTunopbaitap5

---
+++ NguyenHuuTu-Bai5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Bai5
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nguyen Huu Tu // 2415053122346 // 225LTC03");
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Danh sach la:");
+        foreach (var sc in numbers)
+        {
+            Console.Write(sc + " ");
+        }
+        Console.WriteLine();
+        var ds = numbers.Select(p => p * p);
+        Console.WriteLine("Danh sach binh phuong moi so la:");
+        foreach (var sc in ds)
+        {
+            Console.Write(sc + " ");
+        }
+    }
+}
