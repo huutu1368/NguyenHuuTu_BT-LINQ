062b90a0cd62e3e1c149eb0d58a3832326bb3f4c
Huu Tu <huutu1368@gmail.com>
2026-03-20 20:57:28 +0700

    NguyenHuuTUnopbaitap14

---
+++ NguyenHuuTu-Bai14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Score { get; set; }
+}
+
+class Bai14
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nguyen Huu Tu // 2415053122346 // 225LTC03");
+        List<Student> students = new List<Student>() {
+            new Student { Id = 1, Name = "An", Score = 8 },
+            new Student { Id = 2, Name = "Binh", Score = 6 },
+            new Student { Id = 3, Name = "Chi", Score = 9 },
+            new Student { Id = 4, Name = "Dung", Score = 7 } };
+        Console.WriteLine("Danh sach sinh vien:");
+        foreach (Student sv in students)
+            Console.WriteLine($"ID={sv.Id,-5} || Name={sv.Name,-10} || Score={sv.Score,-5}");
+        var svrot = students.Any(sv => sv.Score < 5);
+        if (svrot)
+            Console.WriteLine("Ket Qua: Co sinh vien rot trong danh sach.");
+        else
+            Console.WriteLine("Ket Qua: Khong co sinh vien nao rot.");
+    }
+}
