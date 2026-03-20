1df7927fa39e87d82fc57ea3741fe34be5953cf9
Huu Tu <huutu1368@gmail.com>
2026-03-20 20:52:09 +0700

    NguyenHuuTunopbaitap11

---
+++ NguyenHuuTu-Bai11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Score { get; set; }
+}
+
+class Bai11
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nguyen Huu Tu // 2415053122346 // 225LTC03");
+        List<Student> students = new List<Student>() {
+            new Student { Id = 1, Name = "An", Score = 8 },
+            new Student { Id = 2, Name = "Binh", Score = 6 },
+            new Student { Id = 3, Name = "Chi", Score = 9 },
+            new Student { Id = 4, Name = "Dung", Score = 7 } };
+        Console.WriteLine("Danh sach sinh vien:");
+        foreach (Student sv in students)
+            Console.WriteLine($"ID={sv.Id,-5} || Name={sv.Name,-10} || Score={sv.Score,-5}");
+        var maxscore = students.Max(d => d.Score);
+        var svmax = students.Where(sv => sv.Score == maxscore);
+        Console.WriteLine("Thong tin sinh vien co diem cao nhat:");
+        foreach (Student sv in svmax)
+            Console.WriteLine($"ID={sv.Id,-5} || Name={sv.Name,-10} || Score={sv.Score,-5}");
+    }
+}
